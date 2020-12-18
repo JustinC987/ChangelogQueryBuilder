bfc1fa7bae84449cbd4e0d20f955f552561c66cd
jclappsy@forefrontcorp.com <jclappsy@forefrontcorp.com>
2020-12-18 13:55:56 -0500

    Removed object files

--- queryObject.js
+++
@@ -1,9 +0,0 @@
-class QueryObject{
-    constructor(objectType, externalIds, names){
-        this.objectType =  objectType;
-        this.externalIds = externalIds; 
-        this.names = names; 
-    }
-}
-
-module.exports = QueryObject;
